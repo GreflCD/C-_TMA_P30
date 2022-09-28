f87adb42bfa50f0a909ade0cec6d96969a8c3d93
Турундаев Максим П-30 <maks.turundaev@bk.ru>
2022-09-29 00:38:11 +0300

    TMA P30

--- TMA_P30_LB4.2/TMA_P30_LB4.2.cpp
+++ TMA_P30_LB4.2/TMA_P30_LB4.2.cpp
@@ -4,33 +4,76 @@
 
 int input()
 {
+    std::cout << "Введите 3 разных числа без пробелов, точек и запятых, нельзя начинать с нуля: " << std::endl;
     int chisla;
     std::cin >> chisla;
-    if (chisla % 10 == chisla % 100 || chisla % 1000 == chisla % 100 || chisla % 10 == chisla % 1000)
+    if (chisla < 100 || chisla > 1000)
     {
-        std::cout << "Вы ввели одинаковые числа, пожалуйста введите заново... ";
+        std::cout << "Вы ввели больше, либо меньше 3 чисел, либо начали с нуля, пожалуйста введите заново... " << std::endl;
+        input();
+    }
+    if (chisla % 10 == div(chisla % 100, 10).quot || div(chisla % 100, 10).quot == div(chisla, 100).quot || chisla % 10 == div(chisla, 100).quot)
+    {
+        std::cout << "Вы ввели одинаковые числа, пожалуйста введите заново... " << std::endl;
         input();
     }
     return chisla;
 }
 
-schet(int chisla)
+int schet(int chisla)
 {
-    int k;
-    if (chisla % 100 > chisla % 10 > chisla % 100)
+    int k(0);
+    if (div(chisla, 100).quot > chisla % 10 && chisla % 10 > div(chisla % 100, 10).quot)
+    {
         k = chisla % 10;
+    }
+
+    if (div(chisla % 100, 10).quot > chisla % 10 && chisla % 10 > div(chisla, 100).quot)
+    {
+        k = chisla % 10;
+    }
+
+    if (chisla % 10 > div(chisla % 100, 10).quot && div(chisla % 100, 10).quot > div(chisla, 100).quot)
+    {
+        k = div(chisla % 100, 10).quot;
+    }
+
+    if (div(chisla, 100).quot > div(chisla % 100, 10).quot && div(chisla % 100, 10).quot > chisla % 10)
+    {
+        k = div(chisla % 100, 10).quot;
+    }
+
+    if (chisla % 10 > div(chisla, 100).quot && div(chisla, 100).quot > div(chisla % 100, 10).quot)
+    {
+        k = div(chisla, 100).quot;
+    }
+
+    if (div(chisla % 100, 10).quot > div(chisla, 100).quot && div(chisla, 100).quot > chisla % 10)
+    {
+        k = div(chisla, 100).quot;
+    }
+    std::cout << k;
+    return k;
+}
+
+int output(int itog)
+{
+    std::cout << "Среднее число: " << itog;
+    return itog;
 }
 
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    std::cout << "Введите 3 разных числа без пробелов, точек и запятых: ";
+
     int chisla;
     chisla = input();
+
     int itog;
     itog = schet(chisla);
 
+    output(itog);
     return 0;
 }
 
