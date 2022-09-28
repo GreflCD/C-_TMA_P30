b194a30e335616726f6819f63e52e6fbc37190d6
Турундаев Максим П-30 <maks.turundaev@bk.ru>
2022-09-28 20:59:07 +0300

    C++ LB TMA 1-4

---
+++ TMA_P30_LB4.1/TMA_P30_LB4.1.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <math.h>
+#include <windows.h>
+
+
+double input_dni()
+{
+    std::cout << "Введите срок депозита в днях: ";
+    double dni;
+    std::cin >> dni;
+    if (dni < 0 || dni != (long long)dni)
+    {
+        std::cout << "Введено отрицательное, либо не целочисленное количество дней пожалуйста введите заново..." << std::endl;
+        input_dni();
+    }
+
+    return dni;
+}
+
+double input_sum()
+{
+    std::cout << "Введите сумму депозита: ";
+    double sum;
+    std::cin >> sum;
+    if (sum < 0)
+    {
+        std::cout << "Сумма не может быть отрицательной, пожалуйста введите заново..." << std::endl;
+        input_sum();
+    }
+    return sum;
+}
+
+double schet6(int dni, double sum)
+{
+    double itog6;
+    itog6 = (sum * dni * 0.06) / 365;
+    itog6 = itog6 / div(dni, 30).quot;
+    return itog6;
+}
+
+double schet8(int dni, double sum)
+{
+    double itog8;
+    itog8 = (sum * dni * 0.08) / 365;
+    itog8 = itog8 / div(dni, 30).quot;
+    return itog8;
+}
+
+double output(double a, double b)
+{
+    std::cout << "Ежемесячная выплата по депозиту под 6% годовых будет равна: " << a << std::endl;
+    std::cout << "Ежемесячная выплата по депозиту под 8% годовых будет равна: " << b << std::endl;
+    return 0;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << "Рассчёт суммы ежемесячных выплат по депозиту под 6%, 8% годовых." << std::endl;
+    double dni;
+    double sum;
+    dni = input_dni();
+    sum = input_sum();
+    double itog6, itog8;
+    itog6 = schet6(dni, sum);
+    itog8 = schet8(dni, sum);
+    output(itog6, itog8);
+
+    return 0;
+}
---
+++ TMA_P30_LB4.2/TMA_P30_LB4.2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <math.h>
+#include <windows.h>
+
+int input()
+{
+    int chisla;
+    std::cin >> chisla;
+    if (chisla % 10 == chisla % 100 || chisla % 1000 == chisla % 100 || chisla % 10 == chisla % 1000)
+    {
+        std::cout << "Вы ввели одинаковые числа, пожалуйста введите заново... ";
+        input();
+    }
+    return chisla;
+}
+
+schet(int chisla)
+{
+    int k;
+    if (chisla % 100 > chisla % 10 > chisla % 100)
+        k = chisla % 10;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << "Введите 3 разных числа без пробелов, точек и запятых: ";
+    int chisla;
+    chisla = input();
+    int itog;
+    itog = schet(chisla);
+
+    return 0;
+}
+
